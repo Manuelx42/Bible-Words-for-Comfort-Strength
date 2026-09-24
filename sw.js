f0ad007641e94bde55a91ff9ed3990389935ef86
Manuelx42 <kurzhals.manuel@gmail.com>
2026-09-24 14:45:36 +0200

    Content Changes
    New Bible Quotes added
    Added a second Bible translation for EN language

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Cache-first strategy for the app shell so verses and bookmarks are
 // available fully offline, even on a first visit after install.
 
-const CACHE_NAME = "comfort-strength-v7";
+const CACHE_NAME = "comfort-strength-v9";
 
 const APP_SHELL = [
   "./",
